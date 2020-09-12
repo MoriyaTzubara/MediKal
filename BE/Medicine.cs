cfa53d552c58a67f930646fa129fb602ff96113d
MoriyaTzubara <m0533151327@gmail.com>
2020-09-12 21:15:38 +0300

    first BE

---
+++ BE/Medicine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Medicine
+    {
+        public int Id { get; private set; }
+        public string GenericName { get; set; }
+        public string Name { get; set; }
+        public string ActiveIngredients { get; set; }
+        public string ImagePath { get; set; }
+        public string Company { get; set; }
+    }
+}
---
+++ BE/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public UserTypeEnum UserType { get; set; }
+    }
+}
