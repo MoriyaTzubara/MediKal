a91d36ac6471edbe50f4e89cc4ed2f5eafe4cd60
MoriyaTzubara <m0533151327@gmail.com>
2020-09-13 19:43:19 +0300

    UserViewModel

---
+++ MediKal/Models/UserViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BE;
+
+namespace MediKal.Models
+{
+    public class UserViewModel
+    {
+        private User user;
+
+        public UserViewModel(User _user)
+        {
+            user = _user;
+        }
+        
+        public int Id
+        {
+            get { return user.Id; }
+        }
+
+        public string UserName
+        {
+            get { return user.UserName; }
+            set { user.UserName = value; }
+        }
+       
+        public string Mail
+        {
+            get { return user.Mail; }
+            set { user.Mail = value; }
+        }
+
+    }
+}
