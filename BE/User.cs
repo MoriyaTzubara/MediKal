52d067afd8044859ac043028c3fbf57a4a1815c2
MoriyaTzubara <m0533151327@gmail.com>
2020-09-13 19:30:40 +0300

    UserViewModel

--- BE/User.cs
+++ BE/User.cs
@@ -11,6 +11,7 @@ namespace BE
         public int Id { get; private set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string Mail { get; set; }
         public UserTypeEnum UserType { get; set; }
     }
 }
