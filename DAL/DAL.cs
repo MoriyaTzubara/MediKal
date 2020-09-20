a12dd4dfbf22f0e12d26c101cc0fea1c3ed2437e
ratzontalya <57313043+ratzontalya@users.noreply.github.com>
2020-09-21 02:49:43 +0300

    DB

--- DAL/DAL.cs
+++ DAL/DAL.cs
@@ -13,82 +13,114 @@ namespace DAL
     {
         public void AddMedicine(Medicine medicine)
         {
+            try
+            {
                 using (var db = new MediKalDB())
                 {
                     db.Medicines.Add(medicine);
                     db.SaveChanges();
                 }
+            }catch(Exception e) { throw new Exception(e.Message); }
         }
 
         public void AddPatient(Patient patient)
         {
+            try { 
             using (var db = new MediKalDB())
             {
                 db.Patients.Add(patient);
                 db.SaveChanges();
             }
+            }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public void AddPrescription(Prescription prescription)
         {
+            try { 
             using (var db = new MediKalDB())
             {
                 db.Prescriptions.Add(prescription);
                 db.SaveChanges();
+                }
             }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public void AddUser(User user)
         {
+            try { 
             using (var db = new MediKalDB())
             {
                 db.Users.Add(user);
                 db.SaveChanges();
+                }
             }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public void DeleteMedicine(int id)
         {
+            try { 
             using (var db = new MediKalDB())
             {
                 Medicine medicine = db.Medicines.Find(id);
                 db.Medicines.Remove(medicine);
                 db.SaveChanges();
+                }
             }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public void DeletePatient(int id)
         {
+            try { 
             using (var db = new MediKalDB())
             {
                 Patient patient = db.Patients.Find(id);
                 db.Patients.Remove(patient);
                 db.SaveChanges();
+                }
             }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public void DeletePrescription(int id)
         {
+            try { 
             using (var db = new MediKalDB())
             {
                 Prescription prescription = db.Prescriptions.Find(id);
                 db.Prescriptions.Remove(prescription);
                 db.SaveChanges();
+                }
             }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public void DeleteUser(int id)
         {
+            try { 
             using (var db = new MediKalDB())
             {
                 User user = db.Users.Find(id);
                 db.Users.Remove(user);
                 db.SaveChanges();
+                }
             }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public IEnumerable<Medicine> GetMedicines()
         {
+            try { 
             List<Medicine> result = new List<Medicine>();
             using (var db = new MediKalDB())
             {
@@ -98,10 +130,14 @@ namespace DAL
                 }
             }
             return result;
+            }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public IEnumerable<Patient> GetPatients()
         {
+            try { 
             List<Patient> result = new List<Patient>();
             using (var db = new MediKalDB())
             {
@@ -111,10 +147,14 @@ namespace DAL
                 }
             }
             return result;
+            }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public IEnumerable<Prescription> GetPrescriptions()
         {
+            try {
             List<Prescription> result = new List<Prescription>();
             using (var db = new MediKalDB())
             {
@@ -124,10 +164,14 @@ namespace DAL
                 }
             }
             return result;
+            }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public IEnumerable<User> GetUsers()
         {
+            try { 
             List<User> result = new List<User>();
             using (var db = new MediKalDB())
             {
@@ -137,10 +181,14 @@ namespace DAL
                 }
             }
             return result;
+            }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public void UpdateMedicine(Medicine medicine)
         {
+            try { 
             using (var db = new MediKalDB())
             {
                 var tmp = db.Medicines.First(m => m.Id == medicine.Id);
@@ -155,7 +203,10 @@ namespace DAL
                 if (medicine.Name != null)
                     tmp.Name = medicine.Name;
                 db.SaveChanges();
+                }
             }
+            catch (Exception e) { throw new Exception(e.Message); }
+
         }
 
         public void UpdatePatient(Patient patient)
