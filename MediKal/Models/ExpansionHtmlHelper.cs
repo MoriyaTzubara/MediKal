66f7597be058d5e5e184e02fe94ac80779d917c4
MoriyaTzubara <m0533151327@gmail.com>
2020-09-21 15:57:36 +0300

    VIEW MODELS Moriya

---
+++ MediKal/Models/ExpansionHtmlHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediKal.Models
+{
+    public static class ExpansionHtmlHelper
+    {
+        public static MvcHtmlString DisplayImage(this HtmlHelper html, string imgPath)
+        {
+            return new MvcHtmlString($"<img src='{imgPath}' height='150' width='150'/>");
+        }
+    }
+}
