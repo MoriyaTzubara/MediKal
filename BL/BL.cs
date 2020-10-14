d3036214cbcc5265d283250d6177b243c03bb8ce
ratzontalya <57313043+ratzontalya@users.noreply.github.com>
2020-10-14 18:56:21 +0300

    talya finishing

--- BL/BL.cs
+++ BL/BL.cs
@@ -363,7 +363,7 @@ namespace BL
 
         public bool IsId(object idn)
         {
-            if (idn == null || idn == "000000000")
+            if (idn == null || idn.ToString() == "000000000")
                 return false;
             string idnumber = "";
             if (idn is string)
